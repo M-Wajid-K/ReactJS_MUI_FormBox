b2d607f3ed13a4bab43bb56e2b6087819ebd4104
Muhammad Wajid Khan <mwajidkhan.social@gmail.com>
2023-07-05 12:34:35 +0500

    MUI box and TailwindCSS button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { TextField } from "@mui/material";
+import Box from "@mui/material/Box";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-basic"
+          label="First Name"
+          variant="outlined"
+          value={inputText}
+          onChange={handleInputChange}
+        />
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-basic"
+          label="Last Name"
+          variant="outlined"
+          value={inputText}
+          onChange={handleInputChange}
+        />
+      </Box>
+      <button
+        className="bg-blue-500 hover:bg-red-500 text-white font-bold
+       py-2 px-4 rounded transition-colors duration-500 animate-pulse hover:text-black"
+      >
+        Hover me!
+      </button>
     </div>
   );
 }
